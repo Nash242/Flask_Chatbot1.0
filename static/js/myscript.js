12f69ca2e4353ae70663a260c9f2ba386f810f2f
Nimish <62691305+Nash242@users.noreply.github.com>
2024-05-14 10:55:52 +0530

    Script and Myscript updated with latest version

--- static/js/myscript.js
+++ static/js/myscript.js
@@ -1,107 +1,146 @@
 var i = 0;
-var speed = 40;
+var speed = 50;
 
-function addexample(){
-    clicked=true
-    $(".chatbox").append(`<li class="chat outgoing">
-                             <div class="user-que">Request to access Dashboard ?</div>
-                         </li>`)
-    $(".chatbox").append(`<li class="chat incoming" id="example-ans" style="">
-                               <span class="material-symbols-outlined">smart_toy</span>
-                               <div>
-                                  <div class="bot-response"></div>
-                                </div>
-                         </li>`)                  
-    
-    let txt2=`1 . Click on the 'Raise new request' button located on the top right of the page.\n2 . Enter the OHR and click anywhere outside the field box.\n3 . The email will auto-populate once the OHR is entered.\n4 . The 'Read' option is set to Read-only by default for security access.\n5 . Choose the type of data access you need for the dashboard (Financial, Non-Financial, Sales).\n6 . Based on the type selected, fill in the corresponding Security Types and Security Values.\n7 . Ensure all Security Types and their values are filled in. If you need access to all values under a specific type, choose 'ALL'.\n8 . For Financial information, select Actuals/Outlook/OP Plan.\n9 . For Non-Financial information, select Headcount (Genpact and contractors) and Recruitment.\n10 . For Sales information, select Inflow, Booking, and Pipeline details.\nSimilarly You can ask the question in the below provided chat input.\n`              
-    typeWriter(txt2.replace(/\n/g,"<br>"),typingSpeed = 40)
 
-    $('.example-btn').prop('disabled', true).css({
-        'background-color': '#e6878d',
-        'color': 'black'
-    });
-}
 
-function typeWriter(text,typingSpeed = 40) {
-    console.log(text);
-    let ele=document.getElementsByClassName("bot-response")
-    let previoustext=ele[ele.length-1].innerHTML
-    let displayedText = "";
-    let i = 0;
-    function type() {
-      if (i < text.length) {
-       // console.log('i',i);
-        displayedText =previoustext + text.substring(0, i + 1);
-        ele[ele.length-1].innerHTML = displayedText;
-        i++;
-        if(i== text.length){
-            $('.like-btn-parent').last().css('display', 'block');
-        }
-        setTimeout(type, typingSpeed);
-      }
-    }
-    type();
-  }
-
-function like(){
+function like() {
     var lastOutgoingText = $('.u-question:last').text().trim();
     var lastBotResponseText = $('.bot-response:last').text().trim();
     var formData = new FormData();
     formData.append('usermassage', lastOutgoingText);
     formData.append('botresponse', lastBotResponseText);
     formData.append('datetime', realtime_datetime());
+    console.log(lastOutgoingText);
+    console.log(lastBotResponseText);
+    console.log(realtime_datetime());
     $.ajax({
         url: '/likeresponse',
         type: 'POST',
         data: formData,
         processData: false,
         contentType: false,
-        success: function(response) {
+        success: function (response) {
             console.log(response);
             $('#res-alert').fadeIn().delay(3000).fadeOut();
             $('.like-btn-parent').css({
                 'opacity': '0.6',
                 'pointer-events': 'none'
             });
+            let htmlstring = `<li class="chat incoming" style="padding-top: 10px;">
+                                <span class="material-symbols-outlined" style="color: transparent;background-color: transparent;">smart_toy</span>
+                                <div class="bot-response">
+                                <div>"Thank you for your feedback !" Your response has been saved.<br>
+                                Let me know if you have any questions...</div>
+                                </div>
+                            </li>`
+            $(".chatbox").append(htmlstring)
         },
-        error: function(xhr, status, error) {
+        error: function (xhr, status, error) {
             console.error(error);
         }
     });
 }
-function dislike(){
+function dislike() {
     var lastOutgoingText = $('.u-question:last').text().trim();
     var lastBotResponseText = $('.bot-response:last').text().trim();
-        var formData = new FormData();
-        formData.append('usermassage', lastOutgoingText);
-        formData.append('botresponse', lastBotResponseText);
-        formData.append('datetime', realtime_datetime());
-        $.ajax({
-            url: '/dislikeresponse',
-            type: 'POST',
-            data: formData,
-            processData: false,
-            contentType: false,
-            success: function(response) {
-                console.log(response);
-                $('#res-alert').fadeIn().delay(3000).fadeOut();
-                $('.like-btn-parent').css({
-                    'opacity': '0.6',
-                    'pointer-events': 'none'
-                });
-            },
-            error: function(xhr, status, error) {
-                console.error(error);
-            }
-        });
+    var formData = new FormData();
+    formData.append('usermassage', lastOutgoingText);
+    formData.append('botresponse', lastBotResponseText);
+    formData.append('datetime', realtime_datetime());
+    console.log(lastOutgoingText);
+    console.log(lastBotResponseText);
+    console.log(realtime_datetime());
+    $.ajax({
+        url: '/dislikeresponse',
+        type: 'POST',
+        data: formData,
+        processData: false,
+        contentType: false,
+        success: function (response) {
+            console.log(response);
+            $('#res-alert').fadeIn().delay(3000).fadeOut();
+            $('.like-btn-parent').css({
+                'opacity': '0.6',
+                'pointer-events': 'none'
+            });
+            let htmlstring = `<li class="chat incoming" style="padding-top: 10px;">
+                            <span class="material-symbols-outlined" style="color: transparent;background-color: transparent;">smart_toy</span>
+                            <div class="bot-response">
+                            <div>"Thank you for your feedback !" Your response has been saved.<br>
+                            if you have any queries you can contanct to your superiors.feel free to ask any queries ..!</div>
+                            </div>
+                        </li>`
+            $(".chatbox").append(htmlstring)
+        },
+        error: function (xhr, status, error) {
+            console.error(error);
+        }
+    });
+    
 }
 
-function realtime_datetime (){
+function realtime_datetime() {
     var currentDatetime = new Date();
     var formattedDatetime = currentDatetime.toISOString();
-    return formattedDatetime 
+    return formattedDatetime
 
 }
 
 
+function typeWriter(text, typingSpeed = 40) {
+    console.log(text);
+    let ele = document.getElementsByClassName("bot-response")
+    let previoustext = ele[ele.length - 1].innerHTML
+    let displayedText = "";
+    let i = 0;
+    function type() {
+        if (i < text.length) {
+            displayedText = previoustext + text.substring(0, i + 1);
+            ele[ele.length - 1].innerHTML = displayedText;
+            i++;
+            if (i == text.length) {
+                let htmlstring = `<div style="color: grey;font-size: 11px;padding-top: 10px;">${getCurrentDateTime()}</div>`
+                $('.like-btn-parent').before(htmlstring).last().css('display', 'block');
+            }
+            setTimeout(type, typingSpeed);
+        }
+    }
+    type();
+}
 
+function getCurrentDateTime() {
+    const now = new Date();
+    const options = {
+        weekday: 'short', // Short weekday name (e.g., "Fri")
+        month: 'short',   // Short month name (e.g., "May")
+        day: 'numeric',   // Numeric day of the month (e.g., "10")
+        year: 'numeric',  // Full numeric year (e.g., "2024")
+        hour: 'numeric',  // Numeric hour (e.g., "5")
+        minute: '2-digit', // Two-digit minute (e.g., "44")
+        second: '2-digit', // Two-digit second (e.g., "17")
+        hour12: true      // Use 12-hour clock with AM/PM
+    };
+
+    const dateTimeString = now.toLocaleString('en-US', options);
+    return dateTimeString;
+}
+
+
+
+function convertUrlsToLinks(text) {
+    var urlRegex = /(https?:\/\/[^\s()]+)/g;
+    return text.replace(urlRegex, function (url) {
+        return ' <a href="' + url + '" target="_blank">' + url + '</a> ';
+    })
+    // or alternatively
+    // return text.replace(urlRegex, '<a href="$1">$1</a>')
+}
+
+function showhide() {
+    var helpBtns = document.getElementById('helpbtns');
+    if (helpBtns.style.display === 'block') {
+        helpBtns.style.display = 'none';
+    } else {
+        helpBtns.style.display = 'block';
+    }
+}
--- static/js/script.js
+++ static/js/script.js
@@ -4,117 +4,78 @@ const chatbox = document.querySelector(".chatbox");
 const chatInput = document.querySelector(".chat-input textarea");
 const sendChatBtn = document.querySelector(".chat-input span");
 const refreshbtn = document.querySelector(".refresh-btn");
-
 var clicked=false;
 let userMessage = null; // Variable to store user's message
-const API_KEY = "PASTE-YOUR-API-KEY"; // Paste your API key here
 const inputInitHeight = chatInput.scrollHeight;
 
-// const createChatLi = (message, className) => {
-//     const chatLi = document.createElement("li");
-//     chatLi.classList.add("chat", `${className}`);
-//     let chatContent = className === "outgoing" ? `<div class='u-question'></div>` : `<span class="material-symbols-outlined">smart_toy</span><div></div>`;
-//     chatLi.innerHTML = chatContent;
-//     chatLi.querySelector("div").textContent = message;
-//     return chatLi; // 
-// }
+
 
 const generateResponse = (usermsg) => {
-    let html_msg=`<li class="chat outgoing"><div class="u-question">${usermsg}</div></li>`;
-    $(".chatbox").append(html_msg)
-    let loader =`<li class="chat incoming"><span class="material-symbols-outlined">smart_toy</span>
-                    <div>
-                        <div class="bot-response"><div class="bot-loader"></div></div>
-                    </div>
-                </li>`
-    $(".chatbox").append(loader)
-    const API_URL = "/getanswer";
-    const formData = new FormData();
-    formData.append("question", userMessage);
-    $.ajax({
-        url: API_URL,
-        type: "POST",
-        data: formData,
-        processData: false,  
-        contentType: false  
-    }).done(function(data) {
-        console.log(data);
-        $('.incoming:last').remove();
-        var res=''
-        if (!data.message.startsWith("answer:")) {
-            res = data.message.trim() //.replace(/\n/g, " <br> ");
-        } else { 
-            res=data.message.trim()
-        }
-        if(data.status == 'success'){
+    if (usermsg.length > 0){
+        let html_msg=`<li class="chat outgoing"><div class="u-question">${usermsg}</div>
+                         <div style="color: grey;font-size:11px;padding-top: 10px;">${getCurrentDateTime()}</div>
+                      </li>`;
+        $(".chatbox").append(html_msg)
+        let loader =`<li class="chat incoming"><span class="material-symbols-outlined">smart_toy</span>
+                         <div class="bot-loader" style="border-radius:10px 10px 10px 10px;"></div>
+                    </li>`
+        $(".chatbox").append(loader)
+        const API_URL = "/getanswer";
+        const formData = new FormData();
+        formData.append("question", usermsg);
+        $.ajax({
+            url: API_URL,
+            type: "POST",
+            data: formData,
+            processData: false,  
+            contentType: false  
+        }).done(function(data) {
+            console.log(data);
+            $('.incoming:last').remove();
+            var res='';
+            if (!data.message.startsWith("answer:")) {
+                res = data.message.trim();
+            } else {
+                res=data.message.trim()
+            }
             $(".chatbox").append(`<li class="chat incoming"><span class="material-symbols-outlined">smart_toy</span>
                                         <div>
                                             <div class="bot-response"></div>
                                             <div class="like-btn-parent" style="display:none">
-                                            <div style="display: flex;gap: 15px;" id="svg-parent ">
-                                            <svg onclick="like()" class="like-btn" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path fill="#FFD43B" d="M313.4 32.9c26 5.2 42.9 30.5 37.7 56.5l-2.3 11.4c-5.3 26.7-15.1 52.1-28.8 75.2H464c26.5 0 48 21.5 48 48c0 18.5-10.5 34.6-25.9 42.6C497 275.4 504 288.9 504 304c0 23.4-16.8 42.9-38.9 47.1c4.4 7.3 6.9 15.8 6.9 24.9c0 21.3-13.9 39.4-33.1 45.6c.7 3.3 1.1 6.8 1.1 10.4c0 26.5-21.5 48-48 48H294.5c-19 0-37.5-5.6-53.3-16.1l-38.5-25.7C176 420.4 160 390.4 160 358.3V320 272 247.1c0-29.2 13.3-56.7 36-75l7.4-5.9c26.5-21.2 44.6-51 51.2-84.2l2.3-11.4c5.2-26 30.5-42.9 56.5-37.7zM32 192H96c17.7 0 32 14.3 32 32V448c0 17.7-14.3 32-32 32H32c-17.7 0-32-14.3-32-32V224c0-17.7 14.3-32 32-32z"></path></svg>
-                                            <svg onclick="dislike()" style="transform: rotateY(180deg);" class="like-btn" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path fill="#FFD43B" d="M313.4 479.1c26-5.2 42.9-30.5 37.7-56.5l-2.3-11.4c-5.3-26.7-15.1-52.1-28.8-75.2H464c26.5 0 48-21.5 48-48c0-18.5-10.5-34.6-25.9-42.6C497 236.6 504 223.1 504 208c0-23.4-16.8-42.9-38.9-47.1c4.4-7.3 6.9-15.8 6.9-24.9c0-21.3-13.9-39.4-33.1-45.6c.7-3.3 1.1-6.8 1.1-10.4c0-26.5-21.5-48-48-48H294.5c-19 0-37.5 5.6-53.3 16.1L202.7 73.8C176 91.6 160 121.6 160 153.7V192v48 24.9c0 29.2 13.3 56.7 36 75l7.4 5.9c26.5 21.2 44.6 51 51.2 84.2l2.3 11.4c5.2 26 30.5 42.9 56.5 37.7zM32 384H96c17.7 0 32-14.3 32-32V128c0-17.7-14.3-32-32-32H32C14.3 96 0 110.3 0 128V352c0 17.7 14.3 32 32 32z"></path></svg>
-                                            </div>
-                                            <div id="res-alert" style="font-size: 12px;color: #ff555f;display:none;">
-                                                 Response Saved...!
-                                            </div>
-                                            </div>
-                                        </div>
-                                    </li>`)   
-        }else{
-            $(".chatbox").append(`<li class="chat incoming"><span class="material-symbols-outlined">smart_toy</span>
-                                        <div>
-                                            <div class="bot-response error" style="padding: 4px;border-radius: 0px 6px 6px 6px;"></div>
-                                            <div class="like-btn-parent" style="display:none">
-                                            <div style="display: flex;gap: 15px;" id="svg-parent ">
-                                            <svg onclick="like()" class="like-btn" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path fill="#FFD43B" d="M313.4 32.9c26 5.2 42.9 30.5 37.7 56.5l-2.3 11.4c-5.3 26.7-15.1 52.1-28.8 75.2H464c26.5 0 48 21.5 48 48c0 18.5-10.5 34.6-25.9 42.6C497 275.4 504 288.9 504 304c0 23.4-16.8 42.9-38.9 47.1c4.4 7.3 6.9 15.8 6.9 24.9c0 21.3-13.9 39.4-33.1 45.6c.7 3.3 1.1 6.8 1.1 10.4c0 26.5-21.5 48-48 48H294.5c-19 0-37.5-5.6-53.3-16.1l-38.5-25.7C176 420.4 160 390.4 160 358.3V320 272 247.1c0-29.2 13.3-56.7 36-75l7.4-5.9c26.5-21.2 44.6-51 51.2-84.2l2.3-11.4c5.2-26 30.5-42.9 56.5-37.7zM32 192H96c17.7 0 32 14.3 32 32V448c0 17.7-14.3 32-32 32H32c-17.7 0-32-14.3-32-32V224c0-17.7 14.3-32 32-32z"></path></svg>
-                                            <svg onclick="dislike()" style="transform: rotateY(180deg);" class="like-btn" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path fill="#FFD43B" d="M313.4 479.1c26-5.2 42.9-30.5 37.7-56.5l-2.3-11.4c-5.3-26.7-15.1-52.1-28.8-75.2H464c26.5 0 48-21.5 48-48c0-18.5-10.5-34.6-25.9-42.6C497 236.6 504 223.1 504 208c0-23.4-16.8-42.9-38.9-47.1c4.4-7.3 6.9-15.8 6.9-24.9c0-21.3-13.9-39.4-33.1-45.6c.7-3.3 1.1-6.8 1.1-10.4c0-26.5-21.5-48-48-48H294.5c-19 0-37.5 5.6-53.3 16.1L202.7 73.8C176 91.6 160 121.6 160 153.7V192v48 24.9c0 29.2 13.3 56.7 36 75l7.4 5.9c26.5 21.2 44.6 51 51.2 84.2l2.3 11.4c5.2 26 30.5 42.9 56.5 37.7zM32 384H96c17.7 0 32-14.3 32-32V128c0-17.7-14.3-32-32-32H32C14.3 96 0 110.3 0 128V352c0 17.7 14.3 32 32 32z"></path></svg>
-                                            </div>
-                                            <div id="res-alert" style="font-size: 12px;color: #ff555f;display:none;">
-                                                 Response Saved...!
-                                            </div>
+                                                <div style="display: flex;gap: 15px;" id="svg-parent">
+                                                <svg onclick="like()" class="like-btn" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path fill="#FFD43B" d="M313.4 32.9c26 5.2 42.9 30.5 37.7 56.5l-2.3 11.4c-5.3 26.7-15.1 52.1-28.8 75.2H464c26.5 0 48 21.5 48 48c0 18.5-10.5 34.6-25.9 42.6C497 275.4 504 288.9 504 304c0 23.4-16.8 42.9-38.9 47.1c4.4 7.3 6.9 15.8 6.9 24.9c0 21.3-13.9 39.4-33.1 45.6c.7 3.3 1.1 6.8 1.1 10.4c0 26.5-21.5 48-48 48H294.5c-19 0-37.5-5.6-53.3-16.1l-38.5-25.7C176 420.4 160 390.4 160 358.3V320 272 247.1c0-29.2 13.3-56.7 36-75l7.4-5.9c26.5-21.2 44.6-51 51.2-84.2l2.3-11.4c5.2-26 30.5-42.9 56.5-37.7zM32 192H96c17.7 0 32 14.3 32 32V448c0 17.7-14.3 32-32 32H32c-17.7 0-32-14.3-32-32V224c0-17.7 14.3-32 32-32z"/></svg>
+                                                <svg onclick="dislike()" style="transform: rotateY(180deg);" class="like-btn" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path fill="#FFD43B" d="M313.4 479.1c26-5.2 42.9-30.5 37.7-56.5l-2.3-11.4c-5.3-26.7-15.1-52.1-28.8-75.2H464c26.5 0 48-21.5 48-48c0-18.5-10.5-34.6-25.9-42.6C497 236.6 504 223.1 504 208c0-23.4-16.8-42.9-38.9-47.1c4.4-7.3 6.9-15.8 6.9-24.9c0-21.3-13.9-39.4-33.1-45.6c.7-3.3 1.1-6.8 1.1-10.4c0-26.5-21.5-48-48-48H294.5c-19 0-37.5 5.6-53.3 16.1L202.7 73.8C176 91.6 160 121.6 160 153.7V192v48 24.9c0 29.2 13.3 56.7 36 75l7.4 5.9c26.5 21.2 44.6 51 51.2 84.2l2.3 11.4c5.2 26 30.5 42.9 56.5 37.7zM32 384H96c17.7 0 32-14.3 32-32V128c0-17.7-14.3-32-32-32H32C14.3 96 0 110.3 0 128V352c0 17.7 14.3 32 32 32z"/></svg>
+                                                </div>
+                                                <div id="res-alert" style="font-size: 12px;color: #ff555f;display:none;">
+                                                    Response Saved...!
+                                                </div>
                                             </div>
                                         </div>
-                                    </li>`)    
-        }
-        res=res.replace(/\n/g,"<br>")
-            console.log(res);
-        typeWriter(res,typingSpeed = 40)
-
-    }).fail(function() {
-        // messageElement.classList.add("error");
-        // messageElement.textContent = "Oops! Something went wrong. Please try again.";
-    }).always(function() {
-        chatbox.scrollTo(0, chatbox.scrollHeight);
-    });
-
+                                    </li>`)
+            res=convertUrlsToLinks(res)
+            res=res.replace(/\n/g, "<br>")
+            res=res.replace('<br><br>','<br>')
+            
+            typeWriter(res,typingSpeed = 40)
+            
+        }).fail(function() {
+            // messageElement.classList.add("error");
+            // messageElement.textContent = "Oops! Something went wrong. Please try again.";
+        }).always(function() {
+            chatbox.scrollTo(0, chatbox.scrollHeight);
+        });
+    }else{
+        alert('Please enter a question...')
+    }
 }
 
 const handleChat = () => {
     $('.like-btn-parent').remove();
-    if (clicked){
-        $('.example-div').remove();
-        $('.chatbox li').eq(0).remove(); 
-        $('.chatbox li').eq(0).remove(); 
-        clicked=false
-    }else{
-        if ($('.example-div').length > 0) {
-            $('.example-div').remove();
-        }
-    }
     userMessage = chatInput.value.trim(); 
-    if(!userMessage) return;
-    // Clear the input textarea and set its height to default
     chatInput.value = "";
     chatInput.style.height = `${inputInitHeight}px`;
     chatbox.scrollTo(0, chatbox.scrollHeight);
-    // setTimeout(() => {
-        // Display "Thinking..." message while waiting for the response
-        // const incomingChatLi = createChatLi("Thinking...", "incoming");
-        // chatbox.appendChild(incomingChatLi);
-        // chatbox.scrollTo(0, chatbox.scrollHeight);
-        generateResponse(userMessage);
-    // }, 600);
+    generateResponse(userMessage);
 }
 
 chatInput.addEventListener("input", () => {
@@ -130,22 +91,34 @@ chatInput.addEventListener("keydown", (e) => {
 });
 
 const refreshchat = () => {
-    if ($('.example-div').length > 0) {
-        $('.example-btn').prop('disabled', false).css({'background-color':'#ff555f','color':'white'});
-        $(".chatbox").html('')  
-    } else {
-        $(".chatbox").html('')
-        let htmlstring=`<div class="example-div">
-                            <span>
-                            click here for demo Q&A
-                            </span>
-                            <button class="example-btn" onclick="addexample()">Example</button>
-                        </div>`
-                    const header = document.querySelector('.chatbot header');
-                    header.insertAdjacentHTML('afterend', htmlstring);
-    }
+    $(".chatbox").html('')  
     console.log('refresh click');
-    
+    chatInput.value = "";
+    chatInput.style.height = `${inputInitHeight}px`;
+    let htmlstring=`<li class="chat incoming"><span class="material-symbols-outlined">smart_toy</span>
+                        <div class="bot-response ">
+                            <div>Hello Buddy,Welcome to Access Robo.What brings you here today? 
+                            Please use the options below or ask me anything about product.</div>
+                            <br>
+                            <br>
+                            <h6>(Do not share any personally identificable information(pii) or sensitive personal information(spi) data as chat history is logged.)</h6>
+                        </div>
+                </li> 
+                <li class="chat incoming" style="padding-top: 10px;">
+                    <span class="material-symbols-outlined" style="color: transparent;background-color: transparent;">smart_toy</span>
+                    <div class="bot-response">
+                    <div>Here are some questions i can help you with.</div>
+                    <button class="bot-btns" onclick="botbtns(this)">What is Azure Key Vault Encryption?</button>
+                    <button class="bot-btns" onclick="botbtns(this)" style="margin: 5px 0px;">How many sessions, a user can open for the same request?</button>
+                    <button class="bot-btns" onclick="botbtns(this)">How is End user login page experience of a Requester different from an Approver?</button>
+                    
+                    </div>
+                </li>
+                <li class="chat incoming" style="padding-top: 10px;">
+                    <span class="material-symbols-outlined" style="color: transparent;background-color: transparent;">smart_toy</span>
+                    <div style="color: grey;font-size: 11px;">Mon, May 13, 2024, 11:42:27 AM</div>
+                </li>  `
+  $(".chatbox").html(htmlstring)
 }
 
 sendChatBtn.addEventListener("click", handleChat);
@@ -155,3 +128,13 @@ refreshbtn.addEventListener("click", refreshchat);
 
 
 
+function botbtns(ele){
+    const buttonText = ele.innerText.trim();
+    $('.like-btn-parent').remove();    
+    generateResponse(buttonText);
+    chatbox.scrollTo(0, chatbox.scrollHeight);
+    const buttons = document.querySelectorAll('.chatbox .bot-btns');
+    buttons.forEach(button => { button.disabled = true });
+    var helpBtns = document.getElementById('helpbtns');
+    helpBtns.style.display = 'none';
+}
